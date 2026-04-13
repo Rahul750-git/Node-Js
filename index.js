4e782219923c3963be52c79593158ad8fbcc5ab7
Rahul750-git <varmarahul08501@gmail.com>
2026-04-13 12:27:57 +0530

    Deploy  file

---
+++ index.js
@@ -0,0 +1,12 @@
+const { urlencoded } = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+app.get("/",function(req,res){
+    res.send("chal raha hai");
+});
