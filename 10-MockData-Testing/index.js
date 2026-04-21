1c1ee2b3443587cb2a7a274998590c98ce29f5b3
Rahul750-git <varmarahul08501@gmail.com>
2026-04-21 19:04:24 +0530

    update code

--- 10-MockData-Testing/index.js
+++ 10-MockData-Testing/index.js
@@ -1,23 +1,31 @@
 const express = require("express");
-const fs = require('fs')
+const fs = require("fs");
 const app = express();
 const users = require("./MOCK_DATA.json");
-const { json } = require("stream/consumers");
 
 const PORT = 3000;
 
-//MiddleWare 
-app.use(express.urlencoded({extended: false}))
+//MiddleWare
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}:${req.ip}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    },
+  );
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`Server is running for http://localhost:${PORT}`);
 });
 
-app.get('/',(req,res)=>{
-  res.send("This is a Home Page")
-})
-
 //RestFul Api
 app.get("/api/users", (req, res) => {
+  // Custom Header
+  res.setHeader("x-myName", "Rahul");
   res.json(users);
 });
 
@@ -31,32 +39,41 @@ app.get("/users", (req, res) => {
 });
 
 //Dynamic Path Parameters
-app.route('/api/users/:id')
-.get((req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find((user)=> user.id === id);
-  res.json(user);
-})
-.patch((req,res)=>{
-    //Edit user with id
-    res.json({status: 'Pending'})
-})
-.delete((req,res)=>{
-    //TODO: delete  the user with id
-    res.json({status:'pending'})
-})
-
-
-app.post('/api/users',(req,res)=>{
-    //TODO: Create new user
-   const  body = req.body;
-  users.push({...body,id: users.length+1})
-  fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-  res.json({status:'success', id: users.length});
-
+    const user = users.find((user) => user.id === id);
+    res.json(user);
   })
-   
-})
+  .patch((req, res) => {
+    //Edit user with id
+    res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    //TODO: delete  the user with id
+    res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //TODO: Create new user
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.Job_Title ||
+    !body.gender
+  ) {
+    return res.status(400).json({ msg: "All fields are require.." });
+  } else {
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      res.status(201).json({ status: "success", id: users.length });
+    });
+  } 
+});
 
 // app.patch('/api/users/:id',(req,res)=>{
 //     //TODO: Edit  the user with id
