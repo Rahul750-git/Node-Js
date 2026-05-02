074c4c09b62917368858fc5c06e59656c94aaf5d
Rahul750-git <varmarahul08501@gmail.com>
2026-05-02 16:38:22 +0530

    create a post field features and update the code

--- 17-mini-project/app.js
+++ 17-mini-project/app.js
@@ -53,9 +53,9 @@ app.post("/login", async (req, res) => {
     if (result) {
       let token = jwt.sign({ email: email, userid: user._id }, "ssssssssssss");
       res.cookie("token", token);
-      res.status(200).redirect("/profile")
-    } else{
-       res.redirect("/login");
+      res.status(200).redirect("/profile");
+    } else {
+      res.redirect("/login");
     }
   });
 });
@@ -64,29 +64,26 @@ app.get("/logout", (req, res) => {
   res.cookie("token", "").redirect("/login");
 });
 
-app.get("/profile",isLoggedIn, async (req, res) => {
-  let user = await userModal.findOne({email: req.user.email })
-  user.populate("posts")
-  res.render('profile',{user})
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModal
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  res.render("profile", { user });
 });
 
-app.get("/post",isLoggedIn, async (req, res) => {
-  let user = await userModal.findOne({email: req.user.email })
-  let {content} = req.body
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModal.findOne({ email: req.user.email });
+  let { content } = req.body;
 
-
- let post = await postModal.create({
+  let post = await postModal.create({
     user: user._id,
-    content 
-  })
- 
+    content,
+  });
 
-  user.post.push(post._id)
- await user.save()
- res.redirect('/profile')
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 });
-
-
 
 function isLoggedIn(req, res, next) {
   if (req.cookies.token === "") {
@@ -97,7 +94,6 @@ function isLoggedIn(req, res, next) {
     next();
   }
 }
-
 
 app.listen(3000, (req, res) => {
   console.log("Server is running  http://localhost:3000");
